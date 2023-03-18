e96c004a2501cf5ef02b9ab10bec7047c0f97a07
Luz Davico <mldavico@gmail.com>
2023-03-18 11:34:33 -0300

    Added free download link

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ btn[0].onclick = function() {
 btn[1].onclick = function() {
     modal[1].style.display = "flex";
 }
+btn[2].onclick = function() {
+    modal[2].style.display = "flex";
+}
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function() {
     modal[0].style.display = "none";
@@ -32,11 +35,16 @@ span[0].onclick = function() {
 span[1].onclick = function() {
     modal[1].style.display = "none";
 }
+span[2].onclick = function() {
+    modal[2].style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal[0]) {
         modal[0].style.display = "none";
     } else if (event.target == modal[1]) {
         modal[1].style.display = "none";
+    } else if (event.target == modal[2]) {
+        modal[2].style.display = "none";
     }
 }
